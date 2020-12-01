c06527b6aa92bbf84814060f8ff40772e08b93a2
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2020-12-01 11:13:54 +0100

    remove the different id

--- src/commands/render.ts
+++ src/commands/render.ts
@@ -32,8 +32,7 @@ export default class Render extends Command {
 
     ramlOBJ.resources.forEach(async (resources: Record<string, any>) => {
       const title = resources.relativeUri.substring(1).replaceAll('/', '_')
-      const nameOfProject = ramlOBJ.title.replaceAll(' ', '_')
-      const ret = await toMarkdown(nameOfProject + '_' + title, ramlOBJ.baseUri, resources)
+      const ret = await toMarkdown(title, ramlOBJ.baseUri, resources)
       try {
         const path = join(flags.out, title + '.md')
         await fsPromises.writeFile(path, ret, {flag: 'w'})
